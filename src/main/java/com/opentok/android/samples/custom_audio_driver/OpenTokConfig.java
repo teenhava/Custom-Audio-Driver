fe40d034aa1617a95197795e6391156c91d307a1
developer <developer@therightdoctors.com>
2017-02-16 13:08:46 +0530

    Himabindhu testing audio

---
+++ src/main/java/com/opentok/android/samples/custom_audio_driver/OpenTokConfig.java
@@ -0,0 +1,17 @@
+package com.opentok.android.samples.custom_audio_driver;
+
+public class OpenTokConfig {
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "45757462";
+    // Replace with a generated Session ID
+    public static final String SESSION_ID = "2_MX40NTc1NzQ2Mn5-MTQ4NzIyOTcyMTgzMn53cmZJZmlmZm9uZ1ZDK2YyRTFTaVU5QU9-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTc1NzQ2MiZzaWc9YjcwMDRjMDc2MTFkZTgyNmM3MDQ5NTMxMGVkZGM2NWNhNzFlYjVkMDpzZXNzaW9uX2lkPTJfTVg0ME5UYzFOelEyTW41LU1UUTROekl5T1RjeU1UZ3pNbjUzY21aSlptbG1abTl1WjFaREsyWXlSVEZUYVZVNVFVOS1mZyZjcmVhdGVfdGltZT0xNDg3MjI5NzYxJm5vbmNlPTAuNzA4NDQxNTY0NjMzMzk3NyZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNDg3MjMzMzU3JmNvbm5lY3Rpb25fZGF0YT1leGFtcGxlJTIwc2Vzc2lvbiUyMGlkJTIwZm9yJTIwdGVsZWNvbnN1bHQ=";
+
+    // Subscribe to a stream published by this client. Set to false to subscribe
+    // to other clients' streams only.
+    public static final boolean SUBSCRIBE_TO_SELF = true;
+}
